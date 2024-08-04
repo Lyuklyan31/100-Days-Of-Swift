452ad75b1f235d514db57a653b3549fea9f2c3ae
oleg <lyuclyan.oleg14@gmail.com>
2024-08-04 13:42:46 +0300

    Make logic

--- GuesstheFlag/GuesstheFlag/MainViewController.swift
+++ GuesstheFlag/GuesstheFlag/MainViewController.swift
@@ -15,10 +15,7 @@ class MainViewController: UIViewController {
     private let buttonThree = UIButton()
     
     var countries = [String]()
-    
-    var correctAnswer: Int {
-         Int.random(in: 0...2)
-    }
+    var correctAnswer = 0
     var score = 0
 
     override func viewDidLoad() {
@@ -27,23 +24,22 @@ class MainViewController: UIViewController {
     }
 
     private func defaultConfiguration() {
-        setupNavigationBar()
         setupUI()
         countriesAppend()
-        askQuestion()
+        setupNavigationBar()
+      
     }
     
     private func setupNavigationBar() {
-            if let navigationBar = self.navigationController?.navigationBar {
-                let appearance = UINavigationBarAppearance()
-                appearance.configureWithDefaultBackground()
-                navigationBar.standardAppearance = appearance
-                navigationBar.scrollEdgeAppearance = appearance
-            }
-            view.backgroundColor = .systemBackground
-            title = countries[correctAnswer].uppercased()
+        if let navigationBar = self.navigationController?.navigationBar {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithDefaultBackground()
+            navigationBar.standardAppearance = appearance
+            navigationBar.scrollEdgeAppearance = appearance
         }
-
+        view.backgroundColor = .systemBackground
+        title = countries[correctAnswer].uppercased()
+    }
     
     private func setupUI() {
         setupButtonOne()
@@ -52,13 +48,10 @@ class MainViewController: UIViewController {
     }
     
     private func setupButtonOne() {
-        buttonOne.setImage(UIImage(named: "us"), for: .normal)
-        buttonOne.imageView?.contentMode = .scaleToFill
+        buttonOne.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         buttonOne.layer.borderWidth = 1
         buttonOne.layer.borderColor = UIColor.lightGray.cgColor
-        
         view.addSubview(buttonOne)
-        
         buttonOne.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
@@ -68,13 +61,10 @@ class MainViewController: UIViewController {
     }
     
     private func setupButtonTwo() {
-        buttonTwo.setImage(UIImage(named: "us"), for: .normal)
-        buttonTwo.imageView?.contentMode = .scaleToFill
+        buttonTwo.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         buttonTwo.layer.borderWidth = 1
         buttonTwo.layer.borderColor = UIColor.lightGray.cgColor
-        
         view.addSubview(buttonTwo)
-        
         buttonTwo.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.bottom.equalTo(buttonOne.snp.top).offset(-25)
@@ -84,13 +74,10 @@ class MainViewController: UIViewController {
     }
     
     private func setupButtonThree() {
-        buttonThree.setImage(UIImage(named: "us"), for: .normal)
-        buttonThree.imageView?.contentMode = .scaleToFill
+        buttonThree.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         buttonThree.layer.borderWidth = 1
         buttonThree.layer.borderColor = UIColor.lightGray.cgColor
-        
         view.addSubview(buttonThree)
-        
         buttonThree.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(buttonOne.snp.bottom).offset(25)
@@ -99,14 +86,35 @@ class MainViewController: UIViewController {
         }
     }
     
-    func countriesAppend() {
-        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "spain", "uk", "us"]
+    @objc func buttonTapped(_ sender: UIButton) {
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
     }
     
-    func askQuestion() {
+    func countriesAppend() {
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "spain", "uk", "us"]
+        askQuestion()
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
+        
+        correctAnswer = Int.random(in: 0...2)
         buttonOne.setImage(UIImage(named: countries[0]), for: .normal)
         buttonTwo.setImage(UIImage(named: countries[1]), for: .normal)
         buttonThree.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
+        
     }
 }
